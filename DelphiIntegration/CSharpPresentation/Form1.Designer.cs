2addf2450f507d58ccca345ea56ca9ed7efee73a
Colin Mullikin <colin.mullikin@sigmanest.com>
2016-09-26 11:58:43 -0400

    Create a FooBar object and call GetFooValue.

--- DelphiIntegration/CSharpPresentation/Form1.Designer.cs
+++ DelphiIntegration/CSharpPresentation/Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnCreateFoobar = new System.Windows.Forms.Button();
+            this.Label_Foo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnCreateFoobar
+            // 
+            this.BtnCreateFoobar.Location = new System.Drawing.Point(12, 12);
+            this.BtnCreateFoobar.Name = "BtnCreateFoobar";
+            this.BtnCreateFoobar.Size = new System.Drawing.Size(103, 23);
+            this.BtnCreateFoobar.TabIndex = 0;
+            this.BtnCreateFoobar.Text = "Create FooBar";
+            this.BtnCreateFoobar.UseVisualStyleBackColor = true;
+            this.BtnCreateFoobar.Click += new System.EventHandler(this.BtnCreateFoobar_Click);
+            // 
+            // Label_Foo
+            // 
+            this.Label_Foo.AutoSize = true;
+            this.Label_Foo.Location = new System.Drawing.Point(185, 17);
+            this.Label_Foo.Name = "Label_Foo";
+            this.Label_Foo.Size = new System.Drawing.Size(37, 13);
+            this.Label_Foo.TabIndex = 1;
+            this.Label_Foo.Text = "Foo = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Label_Foo);
+            this.Controls.Add(this.BtnCreateFoobar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnCreateFoobar;
+        private System.Windows.Forms.Label Label_Foo;
     }
 }
 
--- DelphiIntegration/CSharpPresentation/Form1.cs
+++ DelphiIntegration/CSharpPresentation/Form1.cs
@@ -1,20 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using CLIWrapper;
 
 namespace CSharpPresentation
 {
     public partial class Form1 : Form
     {
+        FooBar myFooBar;
+
         public Form1()
         {
             InitializeComponent();
+            FooBar.Initialize(); 
+
+        }
+
+        ~Form1()
+        {
+            myFooBar.Dispose();
+            FooBar.TearDown();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCreateFoobar_Click(object sender, EventArgs e)
+        {
+            myFooBar = new FooBar();
+            Label_Foo.Text = "Foo = " + myFooBar.GetFooValue();
         }
     }
 }
