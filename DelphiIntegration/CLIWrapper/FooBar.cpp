07979f48a6502fd3239984722ae6b4eb771ee7e7
Colin Mullikin <colin.mullikin@sigmanest.com>
2016-09-26 10:33:19 -0400

    Remove FooBarManager and refactor FooBar to contain its functionality

--- DelphiIntegration/CLIWrapper/FooBar.cpp
+++ DelphiIntegration/CLIWrapper/FooBar.cpp
@@ -1,7 +1,44 @@
 #include "FooBar.h"
 
-FooBar::FooBar(void * aInternalFooBar)
-	:fInternalFooBar(aInternalFooBar)
+FooBar::FooBar()
 {
-	
+	if (NULL != proc_Create)
+	{
+		fInternalFooBar = proc_Create();
+	}
+}
+
+FooBar::~FooBar()
+{
+	if (NULL != proc_Destroy)
+	{
+		proc_Destroy(fInternalFooBar);
+	}
+}
+
+FooBar::!FooBar()
+{
+	if (NULL != proc_Destroy)
+	{
+		proc_Destroy(fInternalFooBar);
+	}
+}
+
+void FooBar::Initialize()
+{
+	libHandle = LoadLibrary(TEXT("DelphiDLL.dll"));
+
+	if (NULL != libHandle)
+	{
+		proc_Create = (CreateFooBar)GetProcAddress(libHandle, "CreateFooBar");
+		proc_Destroy = (DestroyFooBar)GetProcAddress(libHandle, "DestroyFooBar");
+	}
+}
+
+void FooBar::TearDown()
+{
+	if (NULL != libHandle)
+	{
+		FreeLibrary(libHandle);
+	}
 }
--- DelphiIntegration/CLIWrapper/FooBar.h
+++ DelphiIntegration/CLIWrapper/FooBar.h
@@ -1,4 +1,10 @@
 #pragma once
+
+#include <Windows.h>
+
+typedef void* (__stdcall *CreateFooBar)();
+typedef void(__stdcall *DestroyFooBar)(void*);
+
 ref class FooBar
 {
 public:
@@ -6,7 +12,14 @@ public:
 	~FooBar();
 	!FooBar();
 
+	static void Initialize(); //Must call this before FooBar class can be used!
+	static void TearDown(); //Must call this after FooBar class is done being used!
+
 private:
 	void* fInternalFooBar;
+
+	static HINSTANCE libHandle;
+	static CreateFooBar proc_Create;
+	static DestroyFooBar proc_Destroy;
 };
 
